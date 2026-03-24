03373695eb86a77041460c112f432edb43cf4ac1
marioprogram21 <mario13.bilokapic@gmail.com>
2026-03-24 11:01:50 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,22 @@
-const CACHE="app-v1";
-const files=["./","./index.html","./kalkulator-ocjena.html","./kalkulator-opceg.html","./manifest.json","./icon.png"];
-
-self.addEventListener("install",e=>{
-e.waitUntil(caches.open(CACHE).then(c=>c.addAll(files)));
+self.addEventListener('install', event => {
+    console.log('Service Worker installed');
+    event.waitUntil(
+        caches.open('kalkulator-cache-v1').then(cache => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './Kalkulator-ocjena.html',
+                './kalkulator-opceg.html',
+                './icon.png'
+            ]);
+        })
+    );
 });
 
-self.addEventListener("fetch",e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(resp => {
+            return resp || fetch(event.request);
+        })
+    );
 });
